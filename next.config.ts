39968368e7524bccc45e17ca821bd736adca0107
Gustavo Felipe <72261947+Nem-Tudo@users.noreply.github.com>
2026-08-22 00:31:19 -0300

    feat: /bot redirect

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async redirects() {
+    return [
+      {
+        source: "/bot",
+        destination: "https://discord.com/oauth2/authorize?client_id=1540460243270635600",
+        permanent: false,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
